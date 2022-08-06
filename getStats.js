8fb044ff6a4dcf955936b8465ba805365c7adec2
Adrian Dumitru <54896941+adrianeth@users.noreply.github.com>
2022-08-07 00:37:50 +0300

    Add files via upload

---
+++ getStats.js
@@ -0,0 +1,105 @@
+var tokenAddress="0xD723E36fc786Ce5E731218760C805BD0744A8bdc"; 
+var liquidityAddress="0xB7Cf0E0bFc008A787F543E889784a45FB2D430af";
+var WalletAddress="0x000000000000000000000000000000000000dead";
+var WalletAddress2="0x79d9f178135431708c90e60e0180f8b42342a9b9";
+const web3 = new Web3('https://bsc-dataseed1.binance.org:443');
+var tokenPrice = 0
+
+
+var liquidityABI = [{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount0Out","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1Out","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Swap","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint112","name":"reserve0","type":"uint112"},{"indexed":false,"internalType":"uint112","name":"reserve1","type":"uint112"}],"name":"Sync","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"MINIMUM_LIQUIDITY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"burn","outputs":[{"internalType":"uint256","name":"amount0","type":"uint256"},{"internalType":"uint256","name":"amount1","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"factory","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"_reserve0","type":"uint112"},{"internalType":"uint112","name":"_reserve1","type":"uint112"},{"internalType":"uint32","name":"_blockTimestampLast","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_token0","type":"address"},{"internalType":"address","name":"_token1","type":"address"}],"name":"initialize","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"kLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"mint","outputs":[{"internalType":"uint256","name":"liquidity","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"price0CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"price1CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"skim","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount0Out","type":"uint256"},{"internalType":"uint256","name":"amount1Out","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"swap","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"sync","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"token0","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"token1","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]
+
+document.getElementById('fetch').addEventListener('click', ()=> {
+    setTokenPrice();
+    setBurnBalance();
+    setTotalSupply();
+    setMarketcap();
+})
+
+async function setTokenPrice() {
+    var contract = new web3.eth.Contract(liquidityABI, liquidityAddress);
+
+    var tokenLocked = 0;
+    var wbnbLocked = 0;
+    var bnbPrice = 0;
+
+    try {
+        $.ajax({url: "https://www.binance.com/api/v3/avgPrice?symbol=BNBUSDT", async: false, success: function( data ) {
+            bnbPrice = data['price']
+            contract.methods.getReserves().call(function(error, result){
+                tokenLocked = result['_reserve1']*0.00001
+                wbnbLocked = result['_reserve0']*0.000000000000000001
+                
+                tokenPrice = wbnbLocked/tokenLocked*bnbPrice;
+                
+                $('#result').text('$'+tokenPrice.toFixed(8))
+                
+            });
+        }});
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function setBurnBalance() {
+  let tokenContract = new web3.eth.Contract(liquidityABI, tokenAddress);
+
+
+          
+              var WalletTokenBalance = await tokenContract.methods.balanceOf(WalletAddress).call();
+              var WalletTokenBalance2 = await tokenContract.methods.balanceOf(WalletAddress2).call();
+
+              var decimals = await tokenContract.methods.decimals().call();
+              var adjustedBalance = WalletTokenBalance * 10 ** -decimals;
+
+              var decimals2 = await tokenContract.methods.decimals().call();
+              var adjustedBalance2 = WalletTokenBalance2 * 10 ** -decimals2;
+
+              var adjustedBalance3 = adjustedBalance + adjustedBalance2;
+              $('#result2').text(Number(adjustedBalance3.toFixed(0)).toLocaleString()) 
+}
+
+async function setTotalSupply() {
+  let tokenContract = new web3.eth.Contract(liquidityABI, tokenAddress);
+
+
+          
+              var WalletTokenBalance = await tokenContract.methods.totalSupply().call();
+              
+
+              var decimals = await tokenContract.methods.decimals().call();
+              var totalSupply = WalletTokenBalance * 10 ** -decimals;
+
+              var WalletTokenBalance1 = await tokenContract.methods.balanceOf(WalletAddress).call();
+              var WalletTokenBalance2 = await tokenContract.methods.balanceOf(WalletAddress2).call();
+
+              var decimals1 = await tokenContract.methods.decimals().call();
+              var adjustedBalance1 = WalletTokenBalance1 * 10 ** -decimals1;
+
+              var decimals2 = await tokenContract.methods.decimals().call();
+              var adjustedBalance2 = WalletTokenBalance2 * 10 ** -decimals2;
+
+              console.log(totalSupply);
+
+              var circulatingSupply = totalSupply - (adjustedBalance1 + adjustedBalance2)
+              $('#result3').text(Number(circulatingSupply.toFixed(0)).toLocaleString()) 
+
+              var marketcap = totalSupply * tokenPrice;
+  $('#result4').text('$'+Number(marketcap.toFixed(0)).toLocaleString())
+}
+
+
+Number.prototype.toFixedSpecial = function(n) {
+    var str = this.toFixed(n);
+    if (str.indexOf('e+') === -1)
+      return str;
+  
+    // if number is in scientific notation, pick (b)ase and (p)ower
+    str = str.replace('.', '').split('e+').reduce(function(p, b) {
+      return p + Array(b - p.length + 2).join(0);
+    });
+    
+    if (n > 0)
+      str += '.' + Array(n + 1).join(0);
+    
+    return str;
+  };
